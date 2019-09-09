fb4db2785f73c7e98b35df880e8e26ae0bf13021
sanacore <rootforsana@gmail.com>
2019-09-09 22:11:05 +0900

    - html document를 string으로 변환 - 제곱미터 특수문자 크롬 확장프로그램 업로드하면 깨지는 문제가 있어 유니코드 표현으로 변환하여 해결

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
-var tab_title = '';
-
-function display_h1 (results){
-  h1=results;
-  document.querySelector("#id1").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + "</p>";
-}
-
-chrome.tabs.query({active: true}, function(tabs) {
-  var tab = tabs[0];
-  tab_title = tab.title;
-  chrome.tabs.executeScript(tab.id, {
-    code: 'document.querySelector("h1").textContent'
-  }, display_h1);
-});
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+      message.innerText = request.source;
+      console.log("191.49\u33A1")
+      // var regex = new RegExp("[0-9]+(.[0-9]{2})?$");
+      var regex = new RegExp("191.49\u33A1");
+      console.log(message.innerText)
+      var match = regex.exec(message.innerText);
+      console.log(match[0]);   
+    }
+  });
+  
+  function onWindowLoad() {
+  
+    var message = document.querySelector('#message');
+  
+    chrome.tabs.executeScript(null, {
+      file: "getPagesSource.js"
+    }, function() {
+      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      if (chrome.runtime.lastError) {
+        message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+      }
+    });
+  
+  }
+  
+  window.onload = onWindowLoad;
