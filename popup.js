0210b633981ddd47a7ea0dc6bba97d4a111beb48
sanacore <rootforsana@gmail.com>
2019-09-22 21:24:25 +0900

    html 기본 포멧 적용

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+class mTop {
+  constructor(square_meter) {
+      this.square_meter = String(square_meter);
+      this.pyeong = String((Number(square_meter.replace('\u33A1', ''))/3.305785).toFixed(1)+'평');
+  }
+}
+
 chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
     var NUMERIC_REGEXP = /[-]{0,1}[\d]*[\.]{0,1}[\d]+\u33A1/g;
@@ -6,6 +13,19 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     // https://stackoverflow.com/questions/1183903/regex-using-javascript-to-return-just-numbers
     console.log(match);
 
+    for (i = 0; i < match.length; i++) {
+      console.log("match[i]: "+match[i]);
+      var p = new mTop(match[i]);
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(p.square_meter + " " + p.pyeong);
+      console.log("p.pyeong[i]: "+p.pyeong);
+      node.appendChild(textnode);
+      node.classList.add("list-group-item");
+      var element = document.getElementById("mTop");
+      element.appendChild(node);
+    }
+
+    
   }
 });
 
